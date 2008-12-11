31b6bf0ab30f26248dc744919ed9184bf77c259e
vladlosev <vladlosev@861a406c-534a-0410-8894-cb66d6ee9925>
2008-12-11 06:09:38 +0000

    Creates tags/release-1.2.1 from the head of branches/release-1.2

--- test/gtest-param-test_test.cc
+++ test/gtest-param-test_test.cc
@@ -42,6 +42,10 @@
 #include <list>
 #include <vector>
 
+#ifdef GTEST_HAS_COMBINE
+#include <tr1/tuple>
+#endif  // GTEST_HAS_COMBINE
+
 // To include gtest-internal-inl.h.
 #define GTEST_IMPLEMENTATION
 #include "src/gtest-internal-inl.h"  // for UnitTestOptions
